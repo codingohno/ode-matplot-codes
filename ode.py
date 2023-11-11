72939c96447e0b0f9d3964cd45bb84fc7161c521
xijie <ivanlim853@gmail.com>
2023-11-11 21:17:55 +0800

    multiple A inputs, user def A and k

--- ode.py
+++ ode.py
@@ -4,15 +4,24 @@ from scipy.integrate import odeint
 from scipy.integrate import solve_ivp
 from math import sqrt
 
+#getting inputs from user
+A_input=input("what values of A:(ex:1,3,5)").split(',')
+A_input=[int(x) for x in A_input]
+print(A_input)
+
+k_input=input("v value:(ex:1)").split();
+k_input=int(k_input[0])
+
+
 #the parameters that can be changed by user
 #the A value that can be changed by user
-k=2
+k=k_input
 V=1
-A=1
+#resolution
+t = np.linspace(0,3.5,50)#solve for 100 points between 0 to 1
 
 
-#ultily functions
-
+#ultility functions
 #we have a system of first order of ode
 #let us define that s=(x,y) 
 #    | x |        | dx/dt |
@@ -28,52 +37,52 @@ def dsdt(t,s):
     #return the differentiation of the column vector
     return [dxdt,dydt]
 
+def solve_and_plot_single(chosen_A):
+
+    # picking init conditions
+    y0=0
+    x0=chosen_A
+
+    #there are a two types of solver given in the vid
+    # odeint (old solving function:not recommended)
+    # solve_ivp(with inti cond)
+    solution_s=solve_ivp(dsdt,t_span=(0,max(t)),y0=(x0,y0),t_eval=t)
+
+    #extract the x and y values
+    #the T is the transpose 
+    x_arr=solution_s.y[0]
+    y_arr=solution_s.y[1]
 
 
-
-
-#init conditions
-y0=0
-x0=A
-
-#there are a two types of solver given in the vid
-# odeint (without init cond)
-# solve_ivp(with inti cond)
-
-
-#the solving
-t = np.linspace(0,1,100)#solve for 100 points
-
-
-solution_s=solve_ivp(dsdt,t_span=(0,max(t)),y0=(x0,y0),t_eval=t)
-
+    #plot the graph to t(not used for observing position to time relationship)
+    # plt.plot(x_arr,t,'--')
+    # plt.plot(y_arr,t)
+    # plt.plot(human_arr,t)
+    # plt.ylabel('$x or y$',fontsize=22)
+    # plt.xlabel("$t$",fontsize=22)
+    # plt.show()
+    plt.plot(x_arr,y_arr)
 
 
 #comparision for the moving human
+#human always go with 1V
 def dhumandt(t,human_pos):
     return V
 solution_human=solve_ivp(dhumandt,t_span=(0,max(t)),y0=[0],t_eval=t)
-
-#extract the v values
-#the T is the transpose 
-x_arr=solution_s.y[0]
-y_arr=solution_s.y[1]
 human_arr=solution_human.y[0]
-
-#plot the graph to t
-# plt.plot(x_arr,t,'--')
-# plt.plot(y_arr,t)
-# plt.plot(human_arr,t)
-
-# plt.ylabel('$x or y$',fontsize=22)
-# plt.xlabel("$t$",fontsize=22)
-
-# plt.show()
-
-plt.plot(x_arr,y_arr)
 plt.plot(len(human_arr)*[0],human_arr)
 
+#plot the dog
+#different A
+for a_val in A_input:
+    solve_and_plot_single(a_val)
+
+
+
+
+#setting the label for axes
 plt.ylabel('$y$',fontsize=22)
 plt.xlabel("$x$",fontsize=22)
 
+#show the plot
 plt.show()
